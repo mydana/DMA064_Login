ac691a8b44654996538e8f4b4dcb0d8f7bcf0a68
Dana Runge <7285068+mydana@users.noreply.github.com>
2025-04-25 13:29:09 -0700

    Add Programmatic Segues

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -16,8 +16,42 @@ class ViewController: UIViewController {
 
     @IBOutlet var usernameText: UITextField!
 
+    @IBOutlet var forgotUserNameButton: UIButton!
+    
+    @IBAction func forgotUserNameAction(_ sender: Any) {
+        performSegue(withIdentifier: "Forgot", sender: sender)
+    }
+    
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    @IBAction func forgotPasswordAction(_ sender: Any) {
+        performSegue(withIdentifier: "Forgot", sender: sender)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = usernameText.text
+        var title = "Error occured"
+        if let id = segue.identifier {
+            if id == "Forgot" {
+                if let sender = sender as? UIButton {
+                    if sender == forgotPasswordButton {
+                        title = "Forgot Password"
+                    } else if sender == forgotUserNameButton {
+                        title = "Forgot Username"
+                    } else {
+                        title = "Illegal sender"
+                    }
+                } else {
+                    title = "Unknown sender"
+                }
+            } else if id == "Login" {
+                title = usernameText.text!
+            } else {
+                title = "Unknown segue"
+            }
+        } else {
+            title = "Unknown segue identifier"
+        }
+        segue.destination.navigationItem.title = title
     }
 
 }
